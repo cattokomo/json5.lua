42bdecd419f1f3df0db91fa5a0728898e8f72151
komothecat <71205197+komothecat@users.noreply.github.com>
2024-07-28 17:12:10 +0800

    full release: 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # json5.lua
-Pure Lua JSON5 parser and serializer
+Pure Lua [JSON5](https://json5.org) parser and serializer
+
+## Installation & Usage
+
+Put [`json5.lua`](./json5.lua) into your project and then
+```lua
+local json5 = require("json5")
+```
+
+### `json5.encode(value, options)`
+
+Parameters:
+  - `value: any`: A value to be encoded into JSON5 format.
+  - `options: table`:
+    - `explicit_string_key: boolean`: Explictly wrap key in quote regardless if it's a valid JSON5 identifier.
+    - `explicit_positive_sign: boolean`: Explicitly add positive sign prefix to numbers.
+    - `use_single_quote: boolean`: Use single quotes for string instead of double quotes
+    - `json_compatible: boolean`: Enable compatibility with JSON, this will disables JSON5 goodies.
+Returns: `string`
+
+### `json5.decode(str)`
+
+Parameters:
+  - `str: string`: A JSON5 format string.
+Returns: `any`
--- json5.lua
+++ json5.lua
@@ -1,5 +1,13 @@
 local json5 = {}
 
+-- TODO: optimize decode function
+
+---@class json5.encode_options
+---@field explicit_string_key boolean Explictly wrap key in quote regardless if it's a valid JSON5 identifier
+---@field explicit_positive_sign boolean Explicitly add positive sign prefix to numbers
+---@field use_single_quote boolean Use single quotes for string instead of double quotes
+---@field json_compatible boolean Enable compatibility with JSON, this will disables JSON5 goodies
+
 ---@diagnostic disable-next-line: deprecated
 local unpack = unpack or table.unpack
 
@@ -14,6 +22,9 @@ local function create_enum(name)
     })
 end
 
+---@param str string
+---@param ... string
+---@return string
 local function matchs(str, ...)
     local ret = {}
     for i = 1, select("#", ...) do
@@ -26,21 +37,13 @@ local function matchs(str, ...)
     return unpack(ret)
 end
 
+---@param tbl table
+---@return table
 local function copy(tbl)
     local t = {}
     for i, v in pairs(tbl) do
-        t[i] = type(v) == "table" and copy(t) or v
+        t[i] = type(v) == "table" and copy(v) or v
     end
-    return t
-end
-
-local function car(t)
-    return table.remove(copy(t), 1)
-end
-
-local function cdr(t)
-    t = copy(t)
-    table.remove(t, 1)
     return t
 end
 
@@ -70,13 +73,19 @@ end
 ---
 json5.null = create_enum("json5.null")
 
+---@class json5.empty_array
+---
+--- A special enum to encode value as empty JSON array.
+---
+json5.empty_array = create_enum("json5.empty_array")
+
 ---@param tbl {string:any}|any[]
 ---@return {string:any}|any[]
 ---
 --- Return a table with data to encode table as object.
 ---
 function json5.as_object(tbl)
-    return setmetatable(tbl, {
+    return setmetatable(copy(tbl), {
         __json5_type = "object",
     })
 end
@@ -87,7 +96,7 @@ end
 --- Return a table with data to encode table as array.
 ---
 function json5.as_array(tbl)
-    return setmetatable(tbl, {
+    return setmetatable(copy(tbl), {
         __json5_type = "array",
     })
 end
@@ -124,12 +133,28 @@ serializer = setmetatable({
     end,
     ---@param rope string[]
     ---@param value string
-    string = function(rope, value)
-        rope[#rope + 1] = '"' .. value:gsub(escape_patt, escapes) .. '"'
+    ---@param options table
+    string = function(rope, value, options)
+        local quote = '"'
+        if options.use_single_quote then
+            quote = "'"
+        end
+        rope[#rope + 1] = quote .. value:gsub(escape_patt, escapes) .. quote
     end,
     ---@param rope string[]
     ---@param value number
-    number = function(rope, value)
+    ---@param options table
+    ---@return string?
+    number = function(rope, value, options)
+        if
+            options.json_compatible and
+            tostring(value) == "nan" or
+            value == math.huge or
+            value == -math.huge
+        then
+            return "unexpected number value '" .. tostring(value) .. "'"
+        end
+
         if tostring(value) == "nan" then
             rope[#rope + 1] = "NaN"
         elseif value == math.huge then
@@ -137,7 +162,11 @@ serializer = setmetatable({
         elseif value == -math.huge then
             rope[#rope + 1] = "-Infinity"
         else
-            rope[#rope + 1] = tostring(value)
+            local suffix =
+                (value > 0 and options.explicit_plus_sign)
+                and "+"
+                or ""
+            rope[#rope + 1] = suffix .. tostring(value)
         end
     end,
     ---@param rope string[]
@@ -147,15 +176,18 @@ serializer = setmetatable({
     end,
     ---@param rope string[]
     ---@param value table
+    ---@param options table
     ---@return string?
-    table = function(rope, value)
+    table = function(rope, value, options)
         if value == json5.null then
             rope[#rope + 1] = "null"
+            return
+        elseif value == json5.empty_array then
+            rope[#rope + 1] = "[]"
             return
         end
 
         local json5_type = (getmetatable(value) or {}).__json5_type
-
 
         if rawget(value, 1) ~= nil or next(value) == nil then
             json5_type = json5_type or "array"
@@ -169,10 +201,12 @@ serializer = setmetatable({
                 if v == value then
                     return "circular reference"
                 end
-                local err = serializer[type(v)](rope, v)
+
+                local err = serializer[type(v)](rope, v, options)
                 if err and v ~= json5.null then
                     return err
                 end
+
                 rope[#rope + 1] = ","
             end
             rope[#rope] = nil
@@ -182,23 +216,30 @@ serializer = setmetatable({
                     return "circular reference"
                 end
                 if type(k) == "string" then
-                    serializer.string(rope, k)
+                    if options.explicit_string_key or not k:match("^[%a%$_][%w_]*$") then
+                        serializer.string(rope, k, options)
+                    else
+                        rope[#rope + 1] = k
+                    end
                     rope[#rope + 1] = ":"
-                    local err = serializer[type(v)](rope, v)
+                    local err = serializer[type(v)](rope, v, options)
                     if err then
                         return err
                     end
+
                     rope[#rope + 1] = ","
                 end
             end
+
             rope[#rope] = nil
         end
+
         rope[#rope + 1] = json5_type == "array" and "]" or "}"
     end,
 }, serializer_mt)
 
 ---@param value {string:any}|any[]
----@param options? table
+---@param options json5.encode_options?
 ---@return string?
 ---@return string?
 ---
@@ -208,8 +249,14 @@ serializer = setmetatable({
 function json5.encode(value, options)
     options = options or {}
 
+    if options.json_compatible then
+        options.explicit_positive_sign = false
+        options.explicit_string_key = true
+        options.use_single_quote = false
+    end
+
     local rope = {}
-    local err = serializer[type(value)](rope, value)
+    local err = serializer[type(value)](rope, value, options)
 
     if err then
         return nil, err
@@ -510,7 +557,7 @@ local function lex_identifier(tokens, str)
         return str
     end
 
-    if not str:match("^[%a%$]%w?") then
+    if not str:match("^[%a%$_][%w_]?") then
         return str
     end
 
@@ -518,7 +565,7 @@ local function lex_identifier(tokens, str)
     while #str ~= 0 do
         local c
         c, str = getchar(str)
-        if c:match("[^%w%$]") then
+        if c:match("[^%w%$_]") then
             str = c .. str
             break
         end
@@ -552,9 +599,6 @@ local function lex(str)
             end
         else
             ---@diagnostic disable:param-type-mismatch
-            str, err = lex_identifier(tokens, str)
-            if err then return nil, err end
-
             str, err = lex_comment(tokens, str)
             if err then return nil, err end
 
@@ -564,16 +608,19 @@ local function lex(str)
             str, err = lex_string(tokens, str)
             if err then return nil, err end
 
-            str, err = lex_number(tokens, str)
+            str, err = lex_hex(tokens, str)
             if err then return nil, err end
 
-            str, err = lex_hex(tokens, str)
+            str, err = lex_number(tokens, str)
             if err then return nil, err end
 
             str, err = lex_boolean(tokens, str)
             if err then return nil, err end
 
             str, err = lex_null(tokens, str)
+            if err then return nil, err end
+
+            str, err = lex_identifier(tokens, str)
             if err then return nil, err end
             ---@diagnostic enable:param-type-mismatch
         end
@@ -584,6 +631,13 @@ end
 
 ---@alias parse_func fun(tokens: any[]): any?, string|any[]
 
+---@param tbl any
+---@param typ string
+---@return boolean
+local function check_lex_type(tbl, typ)
+    return type(tbl) == "table" and (getmetatable(tbl) or {}).__json5_lex == typ
+end
+
 local parse
 
 ---@type parse_func
@@ -591,40 +645,123 @@ local function parse_array(tokens)
     local t = {}
 
     if tokens[1] == "]" then
-        return t, cdr(tokens)
+        table.remove(tokens, 1)
+        return t, tokens
     end
 
-    local trail = false
     while #tokens ~= 0 do
+        local tok = tokens[1]
+        if tok == "]" then
+            table.remove(tokens, 1)
+            return t, tokens
+        elseif tok == "," then
+            return nil, "only single trailing comma is allowed in array"
+        end
+
         local v
         v, tokens = parse(tokens)
 
-        if type(v) == "table" and (getmetatable(v) or {}).__json5_lex == "identifier" then
+        if check_lex_type(v, "identifier") then
             return nil, "invalid syntax: " .. v[1]
         end
 
         t[#t + 1] = v
 
-        local tok = car(tokens)
+        tok = tokens[1]
         if tok == "]" then
-            return t, cdr(tokens)
-        else
+            table.remove(tokens, 1)
+            return t, tokens
+        elseif tok ~= "," then
+            return nil, "expected comma after value in array"
         end
+
+        table.remove(tokens, 1)
     end
 
     return nil, "expected end-of-array bracket"
 end
 
+---@type parse_func
+local function parse_object(tokens)
+    local t = {}
+
+    if tokens[1] == "}" then
+        table.remove(tokens, 1)
+        return t, tokens
+    end
+
+    while #tokens ~= 0 do
+        local tok = tokens[1]
+        if tok == "}" then
+            table.remove(tokens, 1)
+            return t, tokens
+        elseif tok == "," then
+            return nil, "only single trailing comma is allowed in object"
+        end
+
+        local k = tokens[1]
+
+        if type(k) == "string" then
+            table.remove(tokens, 1)
+        elseif check_lex_type(k, "identifier") then
+            k = k[1]
+            table.remove(tokens, 1)
+        else
+            return nil, "expected key to be string, got " .. type(k)
+        end
+
+        if table.remove(tokens, 1) ~= ":" then
+            return nil, "expected colon after key in object"
+        end
+
+        local v
+        v, tokens = parse(tokens)
+
+        t[k] = v
+
+
+        tok = tokens[1]
+        if tok == "}" then
+            table.remove(tokens, 1)
+            return t, tokens
+        elseif tok ~= "," then
+            return nil, "expected comma after pair in object"
+        end
+
+        table.remove(tokens, 1)
+    end
+
+    return nil, "expected end-of-object bracket"
+end
+
+---@param tokens any[]
+---@return any?
+---@return any[]|string
 function parse(tokens)
-    local tok = car(tokens)
+    local tok = table.remove(tokens, 1)
 
     if tok == "[" then
-        return parse_array(cdr(tokens))
+        return parse_array(tokens)
+    elseif tok == "{" then
+        return parse_object(tokens)
+    elseif check_lex_type(tok, "identifier") then
+        return nil, "unexpected identifer: " .. tok[1]
     else
-        return tok, cdr(tokens)
+        return tok, tokens
     end
 end
 
-json5.test = parse
+function json5.decode(str)
+    local v, err = lex(str)
+    if err then
+        return nil, err
+    end
+    ---@diagnostic disable-next-line: cast-local-type, param-type-mismatch
+    v, err = parse(v)
+    if not v and err then
+        return nil, err
+    end
+    return v
+end
 
 return json5
